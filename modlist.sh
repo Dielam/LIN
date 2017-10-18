fd80f5287074d9e731f0cf2df3e2220c806accaf
Dielam <dielagun@ucm.es>
2017-10-18 15:21:50 +0200

    OLEEEE
    NBA2k17

---
+++ modlist.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#Intalar Ftrace
+#Entramos en el directorio
+cd Documentos/P1/modlist
+#Limpiamos
+make clean
+#Hacemos make
+make
+#Metemos el modulo
+sudo insmod modlist.ko
+#Pruebas
+echo add 10 > /proc/modlist
+echo add 16 > /proc/modlist
+echo add 17 > /proc/modlist
+echo add 1 > /proc/modlist
+echo add 19 > /proc/modlist
+cat /proc/modlist
+echo remove 1 > /proc/modlist
+cat /proc/modlist
+echo cleanup > /proc/modlist
+cat /proc/modlist
+#Sacamos el modulo
+sudo rmmod modlist
